6e73ae5ebc8e7c681d0a3c1f5140d07de6f5bd69
Sanjay Mansel <mansel@Sanjays-MacBook-Air.local>
2023-01-20 21:56:50 +0900

    First production build

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dontenv from "dotenv";
 import colors from "colors";
+import path from "path";
+
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 import productRoutes from "./routes/productRoutes.js";
@@ -16,6 +18,20 @@ app.use(express.json());
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+
+const __dirname = path.resolve();
+if(process.env.NODE_ENV === "prod") {
+  console.log(__dirname)
+
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.sendFile("Server is running!! YAH!!!!!!!!")
+  })
+}
 
 app.use(notFound);
 app.use(errorHandler);
